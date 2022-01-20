b8dbd639c31807638b72d7d18fe5858235e78030
SeungYongShim <syshim@me.com>
2022-01-20 23:36:59 +0900

    333

--- src/JsonBenchmark/JsonDeserializerVsJsonDocument.cs
+++ src/JsonBenchmark/JsonDeserializerVsJsonDocument.cs
@@ -1,16 +1,21 @@
+using System.Text.Json;
 using BenchmarkDotNet.Attributes;
+using JsonBenchmark;
+using JsonBenchmark.Properties;
 
 public class JsonDeserializerVsJsonDocument
 {
+    public string JsonText { get; private set; }
+
     [GlobalSetup]
     public void Setuo()
     {
-        
+        JsonText = Resources.Test;
     }
 
     [Benchmark]
-    public int JsonDeserializer() => 1;
+    public SendMailDto JsonDeserializer() => JsonSerializer.Deserialize<SendMailDto>(JsonText);
 
     [Benchmark]
-    public int JsonDocument() => 1;
+    public SendMailDto JsonDocument() => JsonSerializer.Deserialize<SendMailDto>(JsonText);
 }
